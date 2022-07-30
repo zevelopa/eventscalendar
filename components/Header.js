5d6e634c2a0b90ecf43fc6a97e29b669b7fda9ec
Matthew Chen <me@matthewchen.dev>
2022-07-30 14:47:37 +1000

    push

---
+++ components/Header.js
@@ -0,0 +1,14 @@
+const Header = () => {
+    return (
+        <div className="pt-10 flex flex-col max-w-2xl border-white dark:border-black mx-auto pb-16">
+            <h1 className="font-bold content-center text-3xl md:text-5xl mb-1 text-black dark:text-white">
+            CSSA Events Calendar
+            </h1>
+            <h2 className="text-gray-700 dark:text-gray-200 mb-4 text-xl">
+            Events calendar for upcoming CSSA events and details.
+            </h2>
+        </div>
+    )
+}
+
+export default Header
--- pages/index.js
+++ pages/index.js
@@ -1,21 +1,14 @@
 import Head from 'next/head'
 import Image from 'next/image'
+import Container from "../components/Container";
+import Header from "../components/Header"
 
 export default function Home() {
   return (
-    <div className={""}>
-      <div className="flex flex-col justify-center items-start max-w-2xl border-white dark:border-black mx-auto pb-16">
-        <div className="flex flex-col sm:flex-row items-start">
-          <div className="flex flex-col">
-            <h1 className="font-bold content-center text-3xl md:text-5xl mb-1 text-black dark:text-white">
-              CSSA Events Calendar
-            </h1>
-            <h2 className="text-gray-700 dark:text-gray-200 mb-4">
-              Developer, CompSci and CyberSec student
-            </h2>
-          </div>
-        </div>
-      </div>
-    </div>
+    <Container>
+      <Header>
+        
+      </Header>
+    </Container>
   )
 }
